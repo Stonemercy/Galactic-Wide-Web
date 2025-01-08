3c14c6031f87d84e8338ed4e0ce278b02f3c974e
Lee <lee_pritchard@rocketmail.com>
2025-01-08 19:33:23 +0000

    Rename cog name and add guilds joined tracker

--- cogs/admin/usage_logger.py
+++ cogs/admin/usage_logger.py
@@ -1,17 +1,18 @@
 from datetime import time
-from disnake import AppCmdInter
+from disnake import AppCmdInter, Guild
 from disnake.ext import commands, tasks
 from utils.bot import GalacticWideWebBot
-from utils.embeds import CommandUsageEmbed
+from utils.embeds import UsageEmbed
 
 
-class CommandLoggerCog(commands.Cog):
+class UsageLoggerCog(commands.Cog):
     def __init__(self, bot: GalacticWideWebBot):
         self.bot = bot
-        self.command_report.start()
+        self.guilds_joined = 0
+        self.usage_report.start()
 
     def cog_unload(self):
-        self.command_report.stop()
+        self.usage_report.stop()
 
     @commands.Cog.listener()
     async def on_slash_command(self, inter: AppCmdInter):
@@ -19,14 +20,18 @@ class CommandLoggerCog(commands.Cog):
             self.bot.command_usage[inter.application_command.name] = 0
         self.bot.command_usage[inter.application_command.name] += 1
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: Guild):
+        self.guilds_joined += 1
+
     @tasks.loop(time=time(hour=22, minute=0, second=0))
-    async def command_report(self):
+    async def usage_report(self):
         if self.bot.command_usage == {}:
             return
-        embed = CommandUsageEmbed(self.bot.command_usage)
+        embed = UsageEmbed(self.bot.command_usage, self.guilds_joined)
         await self.bot.moderator_channel.send(embed=embed)
         self.bot.command_usage.clear()
 
 
 def setup(bot: GalacticWideWebBot):
-    bot.add_cog(CommandLoggerCog(bot))
+    bot.add_cog(UsageLoggerCog(bot))
