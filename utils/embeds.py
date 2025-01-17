8eee697b09789be4fe1529f9ac1304b9bbdd34b4
Lee <lee_pritchard@rocketmail.com>
2025-01-17 22:38:31 +0000

    Add personal order update timer to bot dashboard

--- utils/embeds.py
+++ utils/embeds.py
@@ -269,6 +269,7 @@ class BotDashboardEmbed(Embed):
             "All Maps": ("MapCog", "map_poster"),
             "Update data": ("DataManagementCog", "pull_from_api"),
             "Major Order Update": ("AnnouncementsCog", "major_order_updates"),
+            "Personal Order Update": ("PersonalOrderCog", "personal_order_update"),
         }
         update_times = {}
         for label, (cog_name, attribute_name) in cogs.items():
