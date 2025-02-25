fee1690293047a62e3696d785ccd36cb49668a68
Lee <lee_pritchard@rocketmail.com>
2025-02-25 12:59:15 +0000

    Enhance Maps class with detailed docstrings and refactor methods for clarity

--- utils/maps.py
+++ utils/maps.py
@@ -1,5 +1,5 @@
 from math import cos, radians, sin
-from random import randint, uniform
+from random import randint
 from data.lists import faction_colours
 from datetime import datetime
 from disnake import Colour, Embed, File, TextChannel
@@ -8,6 +8,8 @@ from utils.data import Data, Tasks
 
 
 class Maps:
+    """A class for creating Maps"""
+
     def __init__(
         self,
         data: Data,
@@ -16,6 +18,15 @@ class Maps:
         languages_json_list: list[dict],
         target_planet: int = None,
     ):
+        """Creates a base map
+
+        Args:
+            data (Data): The Data to use for the Maps
+            waste_bin_channel (TextChannel): The channel where maps are uploaded to
+            planet_names_json (dict): The JSON for planet name translation
+            languages_json_list (list[dict]): A list of JSON for each language the Maps are requested in
+            target_planet (int, optional): The planet index if the map is being used in the `/planet` command. Defaults to None.
+        """
         self.data = data
         self.waste_bin_channel = waste_bin_channel
         self.planet_names_json = planet_names_json
@@ -37,6 +48,7 @@ class Maps:
         self._generate_base_map()
 
     def _generate_base_map(self):
+        """Generates the base map"""
         with Image.open(fp="resources/map.webp") as background:
             background = self._fill_sectors(background=background)
             self._draw_waypoint_lines(background=background)
@@ -59,6 +71,11 @@ class Maps:
             background.save(fp="resources/base_map.webp")
 
     def _fill_sectors(self, background: Image.Image):
+        """Fills in sectors based on faction presence
+
+        Args:
+            background (Image.Image): The map to edit
+        """
         sector_info = {}
         dim_faction_colour = {
             faction: tuple(int(colour / 2.5) for colour in colours)
@@ -78,22 +95,22 @@ class Maps:
         alpha = background.getchannel(channel="A")
         background = background.convert(mode="RGB")
         for info in sector_info.values():
-            self._fill_sector(background, info, dim_faction_colour)
+            info["faction"] = max(set(info["faction"]), key=info["faction"].count)
+            ImageDraw.floodfill(
+                image=background,
+                xy=info["coords"],
+                value=dim_faction_colour[info["faction"]],
+                thresh=25,
+            )
         background.putalpha(alpha=alpha)
         return background
 
-    def _fill_sector(self, background, sector_info, colours):
-        sector_info["faction"] = max(
-            set(sector_info["faction"]), key=sector_info["faction"].count
-        )
-        ImageDraw.floodfill(
-            image=background,
-            xy=sector_info["coords"],
-            value=colours[sector_info["faction"]],
-            thresh=25,
-        )
-
     def _draw_waypoint_lines(self, background: Image.Image):
+        """Draws the waypoints for each active planet
+
+        Args:
+            background (Image.Image): The map to edit
+        """
         draw_on_background_with_sectors = ImageDraw.Draw(im=background)
         for index, coords in self.planet_coordinates.items():
             for waypoint in self.data.planets[index].waypoints:
@@ -112,6 +129,11 @@ class Maps:
                     continue
 
     def _draw_tasks(self, background: Image.Image):
+        """Draws tasks on the map by circling planets
+
+        Args:
+            background (Image.Image): The map to edit
+        """
         faction_mapping = {
             1: "Humans",
             2: "Terminids",
@@ -153,7 +175,40 @@ class Maps:
                             fill_colour=faction_colours["MO"],
                         )
 
+    def _draw_ellipse(
+        self,
+        draw: ImageDraw.ImageDraw,
+        coords: tuple,
+        fill_colour: tuple,
+        radius: int = 15,
+    ):
+        """Draw an ellipse at a coordinate
+
+        Args:
+            draw (ImageDraw.ImageDraw): The ImageDraw object to use
+            coords (tuple): Where to draw the ellipse
+            fill_colour (tuple): What colour to use
+            radius (int, optional): The radius of the ellipse. Defaults to 15.
+        """
+        draw.ellipse(
+            [
+                (coords[0] - radius, coords[1] - radius),
+                (coords[0] + radius, coords[1] + radius),
+            ],
+            fill=fill_colour,
+        )
+
     def _draw_planets(self, background: Image.Image):
+        """Draws circles where planets should be based on the current owner
+
+        Modifications:
+            Meridia - Made to look like the wormhole
+            Planets with 1240 active effect - Coloured red
+            Planets with 1241 active effect - Coloured red and cracked
+
+        Args:
+            background (Image.Image): The map to edit
+        """
         background_draw = ImageDraw.Draw(im=background)
         for index, coords in self.planet_coordinates.items():
             if index == 64:
@@ -201,7 +256,38 @@ class Maps:
                     ),
                 )
 
+    def _draw_cracks(self, background: Image.Image):
+        """Draws cracks on planets affected with effect 1241
+
+        Args:
+            background (Image.Image): The map to edit
+        """
+        draw = ImageDraw.Draw(background)
+        affected_planets = [
+            planet.index
+            for planet in self.data.planets.values()
+            if 1241 in planet.active_effects
+        ]
+        radius = 11
+        for planet_index in affected_planets:
+            cx, cy = self.planet_coordinates[planet_index]
+            angles = []
+            while len(angles) < 7:
+                candidate = randint(0, 360)
+                if all(abs(candidate - a) >= 30 for a in angles):
+                    angles.append(candidate)
+            for start_angle in angles:
+                step_size = radius * 1.2
+                new_x = cx + int(step_size * cos(radians(start_angle)))
+                new_y = cy + int(step_size * sin(radians(start_angle)))
+                draw.line([(cx, cy), (new_x, new_y)], fill="black", width=randint(3, 4))
+
     def _add_dss_icon(self, background: Image.Image):
+        """Adds the DSS icon above the planet it's orbiting
+
+        Args:
+            background (Image.Image): The map to edit
+        """
         dss_icon = Image.open("resources/DSS.png")
         dss_icon = dss_icon.convert("RGBA")
         dss_coords = (
@@ -211,6 +297,12 @@ class Maps:
         dss_icon = background.paste(dss_icon, dss_coords, dss_icon)
 
     def _draw_arrow(self, background: Image.Image, width: int = 20):
+        """Draw an arrow below a planet
+
+        Args:
+            background (Image.Image): The map to edit
+            width (int, optional): Scales the size of the arrow. Defaults to 20.
+        """
         background_draw = ImageDraw.Draw(im=background)
         target_coords = self.planet_coordinates[self.target_planet]
         background_draw.line(
@@ -241,7 +333,32 @@ class Maps:
             width=width,
         )
 
+    async def localize(self):
+        """Localizes the base map for each language in `self.languages_json_list`, sends it to `self.waste_bin_channel` and adds it to the related embed"""
+        for language_json in self.languages_json_list:
+            language_code = language_json["code"]
+            with Image.open(fp="resources/base_map.webp") as map:
+                self._write_names(
+                    background=map,
+                    language_code=language_json["code_long"],
+                )
+                map.save(fp=f"resources/{language_code}.webp")
+                message = await self.waste_bin_channel.send(
+                    file=File(fp=f"resources/{language_code}.webp")
+                )
+                self.embeds[language_code].set_image(url=message.attachments[0].url)
+                self.embeds[language_code].add_field(
+                    name=f"Updated",
+                    value=f"<t:{int(datetime.now().timestamp())}:R>",
+                )
+
     def _write_names(self, background: Image.Image, language_code: str):
+        """Writes the names of active planets in a specific language
+
+        Args:
+            background (Image.Image): The map to edit
+            language_code (str): The language to use
+        """
         font = ImageFont.truetype("gww-font.ttf", 35)
         background_draw = ImageDraw.Draw(im=background)
         for index, coords in self.planet_coordinates.items():
@@ -260,29 +377,23 @@ class Maps:
                     border_colour=border_colour,
                 )
 
-    def _draw_ellipse(
-        self,
-        draw: ImageDraw.ImageDraw,
-        coords: tuple,
-        fill_colour: tuple,
-        radius: int = 15,
-    ):
-        draw.ellipse(
-            [
-                (coords[0] - radius, coords[1] - radius),
-                (coords[0] + radius, coords[1] + radius),
-            ],
-            fill=fill_colour,
-        )
-
     def _draw_planet_name(
         self,
         background_draw: ImageDraw.ImageDraw,
         planet_name: str,
         coords: tuple,
         font: ImageFont.FreeTypeFont,
-        border_colour: str = "deepskyblue",
+        border_colour: str,
     ):
+        """Draws a planet name at the given coordinates
+
+        Args:
+            background_draw (ImageDraw.ImageDraw): The map to edit
+            planet_name (str): The planet name
+            coords (tuple): The coords to write at
+            font (ImageFont.FreeTypeFont): The font to use
+            border_colour (str): The colour the font's border should be.
+        """
         name_text = planet_name.replace(" ", "\n")
         background_draw.multiline_text(
             xy=coords,
@@ -294,42 +405,3 @@ class Maps:
             align="center",
             spacing=-10,
         )
-
-    def _draw_cracks(self, background: Image.Image):
-        draw = ImageDraw.Draw(background)
-        affected_planets = [
-            planet.index
-            for planet in self.data.planets.values()
-            if 1241 in planet.active_effects
-        ]
-        radius = 11
-        for planet_index in affected_planets:
-            cx, cy = self.planet_coordinates[planet_index]
-            angles = []
-            while len(angles) < 7:
-                candidate = randint(0, 360)
-                if all(abs(candidate - a) >= 30 for a in angles):
-                    angles.append(candidate)
-            for start_angle in angles:
-                step_size = radius * 1.2
-                new_x = cx + int(step_size * cos(radians(start_angle)))
-                new_y = cy + int(step_size * sin(radians(start_angle)))
-                draw.line([(cx, cy), (new_x, new_y)], fill="black", width=randint(3, 4))
-
-    async def localize(self):
-        for language_json in self.languages_json_list:
-            language_code = language_json["code"]
-            with Image.open(fp="resources/base_map.webp") as map:
-                self._write_names(
-                    background=map,
-                    language_code=language_json["code_long"],
-                )
-                map.save(fp=f"resources/{language_code}.webp")
-                message = await self.waste_bin_channel.send(
-                    file=File(fp=f"resources/{language_code}.webp")
-                )
-                self.embeds[language_code].set_image(url=message.attachments[0].url)
-                self.embeds[language_code].add_field(
-                    name=f"Updated",
-                    value=f"<t:{int(datetime.now().timestamp())}:R>",
-                )
