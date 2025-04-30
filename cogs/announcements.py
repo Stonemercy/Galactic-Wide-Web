efe3ddc0b5ef28a57156f3080166291ca0d5ae20
Lee <lee_pritchard@rocketmail.com>
2025-04-30 19:35:48 +0100

    Update briefing event condition to include unassigned events

--- cogs/announcements.py
+++ cogs/announcements.py
@@ -54,7 +54,7 @@ class AnnouncementsCog(commands.Cog):
                 ge
                 for ge in self.bot.data.global_events
                 if ge.assignment_id == self.bot.data.assignment.id
-                or ge.title.upper() == "BRIEFING"
+                or (ge.assignment_id == 0 and ge.title.upper() == "BRIEFING")
             ]
             if mo_briefing_list:
                 mo_briefing = mo_briefing_list[0]
