4365d9288f2d28a86036a730f9a8bf7042f8373d
Lee <lee_pritchard@rocketmail.com>
2025-08-15 15:58:28 +0100

    edit: lower wait_time slightly

--- utils/interface_handler.py
+++ utils/interface_handler.py
@@ -6,7 +6,7 @@ from utils.interactables import WikiButton
 from utils.mixins import ReprMixin
 
 # Time to wait between feature updates to avoid rate limits (in seconds)
-WAIT_TIME = 0.019
+WAIT_TIME = 0.018
 
 
 class InterfaceHandler:
