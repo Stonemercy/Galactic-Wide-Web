be8b441a5ff0d4015f616410ac6ed49d5fbb83d1
Lee <lee_pritchard@rocketmail.com>
2025-08-19 19:56:56 +0100

    fix: removing old code

--- cogs/admin/admin_commands.py
+++ cogs/admin/admin_commands.py
@@ -31,7 +31,6 @@ class AdminCommandsCog(commands.Cog):
                 "Dashboard",
                 "Map",
                 "MO Update",
-                # "PO Update",
             ]
         ),
     ):
