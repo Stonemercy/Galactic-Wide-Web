9ab7be42561f7c2890d86de0a70071e444bcca2c
Lee <lee_pritchard@rocketmail.com>
2025-09-01 16:05:54 +0100

    fix: correct property usage in warfront embed

--- utils/embeds/warfront_all_planets_embed.py
+++ utils/embeds/warfront_all_planets_embed.py
@@ -7,13 +7,13 @@ from utils.mixins import EmbedReprMixin
 class WarfrontAllPlanetsEmbed(Embed, EmbedReprMixin):
     def __init__(self, planets: Planets, faction: str):
         planets_list = sorted(
-            [p for p in planets.values() if p.current_owner == faction],
+            [p for p in planets.values() if p.current_owner.full_name == faction],
             key=lambda planet: planet.stats["playerCount"],
             reverse=True,
         )
         super().__init__(
             title=f"All planets for {faction}",
-            colour=Colour.from_rgb(*Factions.get_from_identifier(name=faction)),
+            colour=Colour.from_rgb(*Factions.get_from_identifier(name=faction).colour),
             description=f"There are **{len(planets_list)}** planets under {faction} control",
         )
         name = "Planets list"
