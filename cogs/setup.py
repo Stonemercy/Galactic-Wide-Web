9fad35832e61fb2bbd21b14f91d6937a6c7ba954
Lee <lee_pritchard@rocketmail.com>
2026-01-02 14:17:30 +0000

    fix: correct key error in setup

--- cogs/setup.py
+++ cogs/setup.py
@@ -441,7 +441,9 @@ class SetupCog(commands.Cog):
                     )
                     self.bot.maps.update_base_map(
                         planets=self.bot.data.formatted_data.planets,
-                        assignments=self.bot.data.formatted_data.assignments["en"],
+                        assignments=self.bot.data.formatted_data.assignments.get(
+                            "en", []
+                        ),
                         campaigns=self.bot.data.formatted_data.campaigns,
                     )
                     self.bot.maps.localize_map(
