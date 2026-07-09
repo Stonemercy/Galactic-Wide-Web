07dd294b6252dbbb64cae9e264be8c1fcf5651dc
Lee <lee_pritchard@rocketmail.com>
2026-07-09 22:42:37 +0100

    edit: refactor Dispatch class to streamline message processing

--- utils/api_wrapper/models/dispatch.py
+++ utils/api_wrapper/models/dispatch.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from re import DOTALL, match
 from utils.functions import arrowhead_format
 from utils.mixins import ReprMixin
 
@@ -12,18 +13,13 @@ class Dispatch(ReprMixin):
         self.published_at: datetime = datetime.fromtimestamp(
             war_start_timestamp + raw_dispatch_data.get("published", 0), tz=timezone.utc
         )
-        self.full_message: str = arrowhead_format(
-            text=raw_dispatch_data.get("message", "")
-        )
-        self.title: str = ""
-        self.description: str = ""
+        self.raw_message = raw_dispatch_data.get("message", "")
+        self.full_message: str = arrowhead_format(text=self.raw_message)
 
-        split_lines = self.full_message.splitlines(True)
-        if len(split_lines) == 1:
-            self.description = split_lines[0]
-        elif split_lines != []:
-            self.title = split_lines[0].replace("*", "")
-            self.description = "".join(split_lines[1:]).strip()
-        else:
-            self.title = "New Dispatch"
-            self.description = self.full_message
+        self.title: str = ""
+        title_match = match(r"^<i=3>(.*?)</i>\s*", self.raw_message, DOTALL)
+        if title_match:
+            self.title = arrowhead_format(title_match.group(1).strip()).lstrip("\n")
+            self.raw_message = self.raw_message[title_match.end() :].lstrip("\n")
+
+        self.description: str = arrowhead_format(self.raw_message)
