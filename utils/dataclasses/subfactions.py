df8a79b711da444f2974a638d05da9ea76f1f60d
Lee <lee_pritchard@rocketmail.com>
2026-07-09 07:58:35 +0100

    edit: add factions to subfactions for quick access to colours

--- utils/dataclasses/subfactions.py
+++ utils/dataclasses/subfactions.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from utils.dataclasses import Faction, Factions
 from utils.emojis import Emojis
 
 
@@ -8,6 +9,7 @@ class Subfaction:
     emoji: str
     division_id: int
     poi_id: int
+    faction: Faction
 
     def __hash__(self):
         return hash(self.division_id)
@@ -21,33 +23,82 @@ class Subfaction:
 @dataclass
 class Subfactions:
     THE_JET_BRIGADE = Subfaction(
-        "JET BRIGADE", Emojis.Subfactions.jet_brigade, 1202, 1203
+        "JET BRIGADE",
+        Emojis.Subfactions.jet_brigade,
+        1202,
+        1203,
+        Factions.automaton,
     )
     PREDATOR_STRAIN = Subfaction(
-        "PREDATOR STRAIN", Emojis.Subfactions.predator_strain, 1243, 1245
+        "PREDATOR STRAIN",
+        Emojis.Subfactions.predator_strain,
+        1243,
+        1245,
+        Factions.terminids,
     )
     SPORE_BURST_STRAIN = Subfaction(
-        "SPORE BURST STRAIN", Emojis.Subfactions.spore_burst_strain, 1244, 1386
+        "SPORE BURST STRAIN",
+        Emojis.Subfactions.spore_burst_strain,
+        1244,
+        1386,
+        Factions.terminids,
     )
     INCINERATION_CORPS = Subfaction(
-        "INCINERATION CORPS", Emojis.Subfactions.incineration_corps, 1248, 1249
+        "INCINERATION CORPS",
+        Emojis.Subfactions.incineration_corps,
+        1248,
+        1249,
+        Factions.automaton,
     )
     THE_GREAT_HOST = Subfaction(
-        "THE GREAT HOST", Emojis.Subfactions.the_great_host, 1269, 1269
+        "THE GREAT HOST",
+        Emojis.Subfactions.the_great_host,
+        1269,
+        1269,
+        Factions.illuminate,
     )
     RUPTURE_STRAIN = Subfaction(
-        "RUPTURE STRAIN", Emojis.Subfactions.rupture_strain, 1303, 1310
+        "RUPTURE STRAIN",
+        Emojis.Subfactions.rupture_strain,
+        1303,
+        1310,
+        Factions.terminids,
     )
     DRAGONROACHES = Subfaction(
-        "DRAGONROACHES", Emojis.Subfactions.dragonroaches, 1306, 1309
+        "DRAGONROACHES",
+        Emojis.Subfactions.dragonroaches,
+        1306,
+        1309,
+        Factions.terminids,
     )
-    HIVE_LORDS = Subfaction("HIVE LORDS", Emojis.Subfactions.hive_lords, 1307, 1308)
-    CYBORGS = Subfaction("CYBORGS", Emojis.Subfactions.cyborgs, 1360, 1361)
+    HIVE_LORDS = Subfaction(
+        "HIVE LORDS",
+        Emojis.Subfactions.hive_lords,
+        1307,
+        1308,
+        Factions.terminids,
+    )
+    CYBORGS = Subfaction(
+        "CYBORGS",
+        Emojis.Subfactions.cyborgs,
+        1360,
+        1361,
+        Factions.automaton,
+    )
     MINDLESS_MASSES = Subfaction(
-        "MINDLESS MASSES", Emojis.Subfactions.mindless_masses, 1377, 1378
+        "MINDLESS MASSES",
+        Emojis.Subfactions.mindless_masses,
+        1377,
+        1378,
+        Factions.illuminate,
     )
     APPROPRIATORS = Subfaction(
-        "APPROPRIATORS", Emojis.Subfactions.appropriators, 1380, 1379
+        "APPROPRIATORS",
+        Emojis.Subfactions.appropriators,
+        1380,
+        1379,
+        Factions.illuminate,
+    
     )
 
     _all: tuple[Subfaction] = (
