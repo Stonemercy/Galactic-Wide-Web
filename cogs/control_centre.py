74f8c058deb36c66e7568156831623787e0d9f98
Lee <lee_pritchard@rocketmail.com>
2026-07-13 14:53:10 +0100

    fix: sort order of checks for control center listener

--- cogs/control_centre.py
+++ cogs/control_centre.py
@@ -83,8 +83,8 @@ class ControlCentreCog(Cog):
     async def on_button_clicks(self, inter: MessageInteraction) -> None:
         if (
             not self.bot.ready
-            or inter.author != inter.message.interaction_metadata.user
             or inter.component.custom_id not in MAIN_BUTTONS
+            or inter.author != inter.message.interaction_metadata.user
             and "control_centre" not in inter.component.custom_id
         ):
             return
