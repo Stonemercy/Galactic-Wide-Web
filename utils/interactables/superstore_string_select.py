9e6f69db866b389991ecdd0ffad0ce87881f170c
Lee <lee_pritchard@rocketmail.com>
2026-08-11 15:58:34 +0100

    fix: forgot to add in choice_name variable

--- utils/interactables/superstore_string_select.py
+++ utils/interactables/superstore_string_select.py
@@ -7,12 +7,13 @@ class SuperstoreStringSelect(StringSelect):
     def __init__(self, super_store_pages: list[Superstore.Page]):
         choices: list[SelectOption] = []
         for page in super_store_pages:
+            choice_name = page.name
             if (
                 amount := len([p for p in super_store_pages if p.name == page.name])
             ) > 1:
                 number = len([c for c in choices if page.name in c.label]) + 1
                 choice_name += f" {number}/{amount}"
-            choice = SelectOption(label=page.name, value=page.id)
+            choice = SelectOption(label=choice_name, value=page.id)
             choices.append(choice)
         super().__init__(
             placeholder="Choose Superstore Page",
